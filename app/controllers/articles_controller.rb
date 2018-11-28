7f46080152c1558fd2f1c6b53b512667f10fd431
the.weakestt <the.weakestt@gmail.com>
2018-11-28 00:09:26 -0600

    Styled Errors and Messages Partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "The Article was created succesfully"
+      flash[:success] = "The Article was created succesfully"
       redirect_to article_path(@article)
     else
       render :new
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "The Article was updated succesfully"
+      flash[:success] = "The Article was updated succesfully"
       redirect_to article_path(@article)
     else
       render :edit
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "The Article was succesfully deleted"
+    flash[:danger] = "The Article was succesfully deleted"
     redirect_to articles_path
   end
 
