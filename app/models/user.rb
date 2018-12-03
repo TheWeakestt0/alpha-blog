ae5437f1bbee6ca9e9bd881c210e084437e29015
the.weakestt <the.weakestt@gmail.com>
2018-12-02 22:14:22 -0600

    Added Secure Password and Attribute to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
             length: { minimum:3, maximum:105 },
             format: { with: VALID_EMAIL_REGEX}
+has_secure_password
 
 
 end
